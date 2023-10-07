ed2e0127a7c2c2e07bfa37c7d46ba29516ca8fbd
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2023-10-07 15:39:01 +0530

    Bottom navigation bar added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:shoes_app/pages/cart_page.dart';
+import 'package:shoes_app/pages/shope_page.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const ShopePgae(),
+    const CartPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          currentIndex: _selectedIndex,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag),
+              label: "Cart",
+            ),
+          ]),
+      body: _pages[_selectedIndex],
+    );
   }
 }
---
+++ lib/pages/shope_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ShopePgae extends StatelessWidget {
+  const ShopePgae({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("this is shope page"),
+    );
+  }
+}
